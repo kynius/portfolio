cbdd649f17a33cc8217ac1fef3c0a808d2a4f540
kynius <gadzinakrzys@gmail.com>
2022-06-08 20:52:06 +0200

    prototype of contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,18 @@
+import {Col, Container, Row} from "react-materialize";
+
 function Contact() {
     return (
         <>
-            cos
+            <Container className={'center'} style={{paddingTop: '200px'}}>
+                <Row>
+                    <Col l={6} m={12} s={12} offset={'l3'}>
+                        <input type={'text'} className={'browser-default center nickname-input'} placeholder={"What's your name?"} defaultValue={""}/>
+                    </Col>  
+                    <Col l={6} m={12} s={12} offset={'l3'} style={{paddingTop: '40px'}}>
+                        <input type={'text'} className={'browser-default center nickname-input'} placeholder={"Let's talk about coffe!"}/>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
